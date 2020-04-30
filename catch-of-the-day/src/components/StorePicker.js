ae549707c8620e94f0d6650ed2f750c787979f0b
AntonioMateoGomez <antonio.mateo@47deg.com>
2020-04-30 11:40:41 +0200

    removes onSubmit

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -3,7 +3,7 @@ import React from 'react';
 class StorePicker extends React.Component {
   render() {
     return (
-      <form className="store-selector" onSubmit={this.goToStore}>
+      <form className="store-selector">
         <h2>Please Enter a Store</h2>
         <input
           type="text" required
